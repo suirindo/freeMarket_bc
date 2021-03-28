4673fc734dcfcb4366aeed74073dbad0be29f1dd
suirindo <45725946+suirindo@users.noreply.github.com>
2021-03-28 19:01:49 +0900

    Update sell.js

--- js/sell.js
+++ js/sell.js
@@ -1,1 +1,12 @@
+// 出品する関数
+function sell() {
+    // テキストボックスから入力内容を取得する
+    var itemName = document.getElementById("itemName").value;
+    var description = document.getElementById("description").value;
+    var price = document.getElementById("price").value;
+    var googleDocID = document.getElementById("googleDocID").value;
+    var IPFSHash = "";
 
+    // コントラクトの呼び出し
+    return contract.methods.sell(itemName, description, price, googleDocID, IPFSHash).send({ from: coinbase });
+}
