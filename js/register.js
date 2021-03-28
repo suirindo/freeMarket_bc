572ed82a11f1c900bf43b92fa100b502260995f0
suirindo <45725946+suirindo@users.noreply.github.com>
2021-03-28 19:01:28 +0900

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,1 +1,9 @@
+//会員登録する関数
+function registerAccount() {
+    //テキストボックスから入力内容を取得する
+    var userName = document.getElementById("userName").value;
+    var userEmail = document.getElementById("userEmail").value;
 
+    //コントラクトの呼び出し
+    return contract.methods.registerAccount(userName, userEmail).send({from: coinbase });
+}
